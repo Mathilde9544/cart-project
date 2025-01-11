94112e2819d5ed84bd0f59677059797dfa0a7778
mathilde renaud <renaud.mathilde95@hotmail.fr>
2025-01-12 00:52:17 +0100

    Prices

--- app/javascript/controllers/count_controller.js
+++ app/javascript/controllers/count_controller.js
@@ -4,21 +4,17 @@ export default class extends Controller {
   static values = {
     quantity: Number,
   }
-  static targets = ["newQuantity", "minusbutton"]
+  static targets = ["newQuantity"]
 
   addproduct() {
     this.quantityValue += 1;
     this.newQuantityTarget.innerText = this.quantityValue;
-
-
   }
 
   substractproduct() {
-
     if (this.quantityValue > 1) {
       this.quantityValue -= 1;
       this.newQuantityTarget.innerText = this.quantityValue;
-      
     }
   }
 }
--- app/javascript/controllers/totalcount_controller.js
+++ app/javascript/controllers/totalcount_controller.js
@@ -1,25 +1,44 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["newTotalQuantity", "minusbutton", "productquantity"]
+  static targets = ["newTotalQuantity", "productquantity", "productprice", "subtotal"]
   static values = {
     newquantity: Number,
     startquantity: Number,
-    productquantity: Number
+    productquantity: Number,
+    productprice: Number
   }
-
 
   addproduct() {
     const totalProductQuantity = this.productquantityTargets.reduce((sum, product) => {
     return sum + parseInt(product.innerText, 10) }, 0);
+    this.newTotalQuantityTarget.innerText = `${totalProductQuantity} items`;
 
-    this.newTotalQuantityTarget.innerText = `${totalProductQuantity} items`
+
+    const subtotalPrice = this.productpriceTargets.reduce((sum, productPrice, index) => {
+      const quantity = parseInt(this.productquantityTargets[index].innerText, 10);
+      const price = parseFloat(productPrice.getAttribute("data-totalcount-productprice-value"))
+      return sum + price * quantity;
+      }, 0);
+
+      this.subtotalTargets.forEach((subtotalTarget) => {
+        subtotalTarget.innerText = `$${subtotalPrice}`;
+      });
   }
 
   substractproduct() {
     const totalProductQuantity = this.productquantityTargets.reduce((sum, product) => {
     return sum + parseInt(product.innerText, 10) }, 0);
+    this.newTotalQuantityTarget.innerText = `${totalProductQuantity} items`;
 
-    this.newTotalQuantityTarget.innerText = `${totalProductQuantity} items`
+    const subtotalPrice = this.productpriceTargets.reduce((sum, productPrice, index) => {
+      const quantity = parseInt(this.productquantityTargets[index].innerText, 10);
+      const price = parseFloat(productPrice.getAttribute("data-totalcount-productprice-value"))
+      return sum + price * quantity;
+      }, 0);
+
+    this.subtotalTargets.forEach((subtotalTarget) => {
+      subtotalTarget.innerText = `$${subtotalPrice}`;
+    });;
   }
 }
